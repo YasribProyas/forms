27bc3e8dc687584060143b3aa536034b9b6368d1
proyas21 <abyashrirproyas@gmail.com>
2023-12-08 18:09:39 +0600

    basic appshell setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 import "@mantine/core/styles.css";
-import { MantineProvider } from "@mantine/core";
+import { AppShell, Input, MantineProvider } from "@mantine/core";
 import { theme } from "./theme";
 
 export default function App() {
-  return <MantineProvider theme={theme}>
-    <h1>Form</h1>
-    
-  </MantineProvider>;
+  return (
+    <MantineProvider theme={theme}>
+      <AppShell
+        header={{ height: 90 }}
+        padding={"md"}
+        navbar={{ breakpoint: "sm", width: 250 }}
+      >
+        <AppShell.Header pl="xl">
+          <h1>Form</h1>
+        </AppShell.Header>
+        <AppShell.Navbar>
+          <h3>Yo yo chiki chiki</h3>
+        </AppShell.Navbar>
+        <AppShell.Main>
+          <AppShell.Section>
+            <p>now this is a section</p>
+          </AppShell.Section>
+          <Input.Wrapper title="He he ha ha">
+            <Input placeholder="Name" />
+          </Input.Wrapper>
+        </AppShell.Main>
+      </AppShell>
+    </MantineProvider>
+  );
 }
